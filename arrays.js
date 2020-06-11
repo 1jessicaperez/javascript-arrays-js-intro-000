e470533b40753088a9eaa6c1cbcea999233dbfca
Jessica Perez <1jessicaperez@gmail.com>
2020-06-11 18:30:57 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,11 +1,21 @@
 var chocolateBars= ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element){
-  newArray= [element, ...array]
+  var newArray= [element, ...array]
   return newArray
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
   array.unshift(element)
+  return array
+}
+
+function addElementToEndOfArray(array, element){
+  var newArray= [...array, element]
+  return newArray
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array.shift(element)
   return array
 }
